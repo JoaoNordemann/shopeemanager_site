78170c2ab0ec1f837e3455b8a0c81229781df043
jnordemann <163065421+JoaoNordemann@users.noreply.github.com>
2026-08-30 18:34:15 -0300

    Update DOWNLOAD_URL for Shopee Manager

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@
  * ou use um bundler como Vite/esbuild. O index.html referencia "main.js".
  */
 
-const DOWNLOAD_URL = "https://github.com/SEU_USUARIO/SEU_REPOSITORIO/releases/latest/download/ShopeeManager-Setup.exe";
+const DOWNLOAD_URL = "https://github.com/joaonordemann/shopee-manager-releases/releases/latest/download/Shopee.Manager.exe";
 
 /**
  * Gate de acesso (usuário + senha)
